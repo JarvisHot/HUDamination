4580b9c974f7fd3b2a6b5ebdec4259ab103f9baa
jarvis jiang <jarvisjiang@jarvisdeMacBook-Pro.local>
2016-07-25 20:44:33 +0800

    提交代码

---
+++ HUDAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HUDAnimation
+//
+//  Created by jarvis jiang on 16/7/19.
+//  Copyright © 2016年 jarvis jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HUDAnimation/ViewController.m
@@ -0,0 +1,50 @@
+//
+//  ViewController.m
+//  HUDAnimation
+//
+//  Created by jarvis jiang on 16/7/19.
+//  Copyright © 2016年 jarvis jiang. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MBProgressHUD.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    MBProgressHUD*hud=[MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode=MBProgressHUDModeCustomView;
+    UIImageView *imageView = [[UIImageView alloc] init];
+
+    //创建一个数组，数组中按顺序添加要播放的图片（图片为静态的图片）
+    NSMutableArray *imgArray = [NSMutableArray array];
+    for (int i=1; i<4; i++) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"dropdown_anim__000%d.png",i]];
+        [imgArray addObject:image];
+    }
+    //把存有UIImage的数组赋给动画图片数组
+    imageView.animationImages = imgArray;
+    //设置执行一次完整动画的时长
+    imageView.animationDuration = 3*0.15;
+    //动画重复次数 （0为重复播放）
+    imageView.animationRepeatCount = 0;
+    //开始播放动画
+//    hud.bezelView.backgroundColor=[UIColor clearColor];
+    [imageView startAnimating];
+//    hud.margin=0;
+//    hud.label.backgroundColor=[UIColor clearColor];
+    hud.customView=imageView;
+    hud.label.text=@"heheda";
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
